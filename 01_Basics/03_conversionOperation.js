5b9966ba5408b4321cd481a952dab8d4c26d3cf8
HarshGit-coder <harshmendhi2112@gmail.com>
2025-01-09 11:33:12 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = undefined
+
+// console.log(typeof score);
+//now i want to convert it to number ..
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+//NaN means not a number so it is important to check always.. 
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+//null => 0
+//undefined =>NaN
+
+
+let isLoggedIn = "aniket"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true
+//"" => false
+//"aniket" => true
+
+
+let someNumber = 33;
+let stringsomeNumber = String(someNumber)
+console.log(stringsomeNumber)
