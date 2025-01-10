8124494f9bfb9e250d7e0aa385df3e5c5752a2ef
HarshGit-coder <harshmendhi2112@gmail.com>
2025-01-10 11:04:13 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+
+// console.log(null > 0);false
+// console.log(null == 0);false
+// console.log(null >= 0);true
+
+
+//console.log(undefined > 0); false
+//console.log(undefined < 0); false
+//console.log(undefined == 0); false
+
+// console.log("2" == 2);
+// console.log("2" >= 2);
+// console.log("2" < 2);
+
+//in comparison when we compare different datatypes it is complex problem or issue..
+
+//please avoid the comparison of two different datatypes.. 
