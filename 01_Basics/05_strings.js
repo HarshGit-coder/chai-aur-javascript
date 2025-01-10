9c6f694ac0e65d56205a00bbef9372cb60a4817e
HarshGit-coder <harshmendhi2112@gmail.com>
2025-01-10 11:04:40 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Aniket"
+const repoCount = 4 
+
+// console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+
+
+const studentName = new String ('om-Saraf-com');
+// anoter way to declare string
+
+// console.log(studentName[0]);
+// console.log(studentName.__proto__);
+// console.log(studentName.length);
+// console.log(studentName.toUpperCase());
+// console.log(studentName.charAt(4));
+// console.log(studentName.indexOf('S'));
+console.log(studentName.split('-'));
+
+const newString = studentName.substring(0, 2);
+// console.log(newString);
+const anotherString = studentName.slice(-7, 4)
+// console.log(anotherString);
+
+
+const newStringOne = "    Aniket    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://aniketninave.com/aniket%10ninave";
+// console.log(url.replace('%10', '-'));
+// console.log(url.includes('aniket'));
+// console.log(url.includes('anil'));
+
+
+//it is a method and like this there are many methods or functions... 
