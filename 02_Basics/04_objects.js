106b8bbf3f1a1ada49cc3c293b21294dcb425775
HarshGit-coder <harshmendhi2112@gmail.com>
2025-01-11 19:16:20 +0000

    objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -19,4 +19,36 @@ const normalUser = {
 
 // console.log(normalUser.fullname.userfullname.firstname);
 
-const obj1 = {1: }
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "c", 6: "d"}
+
+// console.log(Object.assign(obj1, obj2, obj3))
+
+
+const users = [
+    {
+        id : 1,
+        email: "a@mail.com" 
+    },
+    {
+        id : 2,
+        email: "d@mail.com" 
+    },
+    {
+        id : 3,
+        email: "c@mail.com" 
+    },
+    {
+        id : 4,
+        email: "b@mail.com" 
+    }
+] 
+// console.log(users[1].email);
+
+// console.log(tinderUser2);
+
+// console.log(Object.keys(tinderUser2))
+// console.log(Object.values(tinderUser2))
+// console.log(Object.entries(tinderUser2))
+//it converts object keyValue pairs to array.. 
