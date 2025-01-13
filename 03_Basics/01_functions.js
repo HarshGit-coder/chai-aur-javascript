28ff609c825bb19afd3f796151cab6e9b339397b
HarshGit-coder <harshmendhi2112@gmail.com>
2025-01-13 11:27:35 +0000

    function

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,54 @@
+//functions are simply meant as whatever the code you write is wrapped in a package..
+
+function sayMyName(){
+    console.log("H")
+    console.log("a")
+    console.log("r")
+    console.log("s")
+    console.log("h")
+}
+// sayMyName()
+
+// function addTwoNumbers (number1, number2)//paramters
+// {
+//     console.log(number1 + number2)//arguments
+// }
+//if we do console value cannot be stored in variable or cannot be returned..  
+
+function addTwoNumbers (number1, number2)
+{
+    let result = number1 + number2
+    return result;
+}
+
+const result= addTwoNumbers(3, 4)
+// console.log("Result: ", result);
+
+
+
+function loginUserMessage(username){
+    if(username === undefined) {
+        console.log("Please Enter Username ")
+    }
+    else{
+           return `${username} just Logged In`
+    }
+    
+}
+
+// console.log(loginUserMessage("Harsh"));
+
+
+
+
+function calculatCartPrice(...num1){
+        return num1
+}
+console.log(calculatCartPrice(200, 4000, 400,200,700))
+// ... triple dots are set to be as rest operator and spread operator it can multiple numbers as parameter and argument
+
+
+const user = {
+    username: "Aniket",
+    price: 199, 
+}
